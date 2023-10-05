6aafbc83a3f9dc7f0808c2342a4cfc1f80001ff7
tmd2809 <tangminhduy2809@gmail.com>
2023-10-05 21:14:12 +0700

    update code

--- index.js
+++ index.js
@@ -5,43 +5,52 @@
  *
  * Please edit webhookUrl and token before run this script!!!
  */
-import TelegramBot from 'node-telegram-bot-api';
-import fetch from 'node-fetch';
-import 'dotenv/config';
+import TelegramBot from "node-telegram-bot-api";
+import fetch from "node-fetch";
+import "dotenv/config";
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.onText(/\/add (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
 
-    if (!match[1].includes('|')) {
-        bot.sendMessage(chatId, 'Vui lòng nhập đúng định dạng.' + '\n\n' + 'Ví dụ:\n```\n/add name|email\n```', {
-            parse_mode: 'Markdown'
-        });
+    if (!match[1].includes("|")) {
+        bot.sendMessage(
+            chatId,
+            "Vui lòng nhập đúng định dạng." +
+                "\n\n" +
+                "Ví dụ:\n```\n/add name|email\n```",
+            {
+                parse_mode: "Markdown",
+            }
+        );
         return;
     }
 
-    bot.sendChatAction(chatId, 'typing');
+    bot.sendChatAction(chatId, "typing");
 
     const resp = match[1];
-    const values = resp.split('|');
+    const values = resp.split("|");
 
     const url = new URL(process.env.WEBHOOK_URL);
-    url.searchParams.append('name', values[0]);
-    url.searchParams.append('email', values[1]);
+    url.searchParams.append("name", values[0]);
+    url.searchParams.append("email", values[1]);
 
     fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            if (data.status === 'success') {
-                bot.sendMessage(chatId, '✅ Đã thêm thành công.');
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status === "success") {
+                bot.sendMessage(chatId, "✅ Đã thêm thành công.");
             } else {
-                bot.sendMessage(chatId, 'Không thể thêm. Vui lòng thử lại sau!');
+                bot.sendMessage(
+                    chatId,
+                    "Không thể thêm. Vui lòng thử lại sau!"
+                );
             }
         })
-        .catch(err => {
-            bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+        .catch((err) => {
+            bot.sendMessage(chatId, "Đã có lỗi xảy ra. Vui lòng thử lại sau!");
         });
 });
 
-console.log('Bot is running...')
+console.log("Bot is running...");
